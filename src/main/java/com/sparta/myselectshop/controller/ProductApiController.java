0b7e064408b18688005b6bf46ed3ad5003a67b46
moonbyeongmin <qudalsrnt3x@gmail.com>
2024-11-05 13:05:21 +0900

    관심 상품 등록 API 구현

---
+++ src/main/java/com/sparta/myselectshop/controller/ProductApiController.java
@@ -0,0 +1,25 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@RestController
+public class ProductApiController {
+
+    private final ProductService productService;
+
+    // 관심 상품 등록하기
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
+        // 응답 보내기
+        return productService.createProduct(requestDto);
+    }
+}
