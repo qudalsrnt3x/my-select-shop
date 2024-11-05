722fa498254f822ac8515125ff16b2f9166c83f9
moonbyeongmin <qudalsrnt3x@gmail.com>
2024-11-05 15:59:45 +0900

    관심 상품에 폴더 추가 및 폴더 전체 조회 API 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductApiController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductApiController.java
@@ -22,7 +22,8 @@ public class ProductApiController {
 
     // 관심 상품 등록하기
     @PostMapping("/products")
-    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto,
+                                            @AuthenticationPrincipal UserDetailsImpl userDetails) {
         // 응답 보내기
         return productService.createProduct(requestDto, userDetails.getUser());
     }
@@ -30,17 +31,25 @@ public class ProductApiController {
     // 관심 상품 조회하기
     // 관심 상품 조회하기
     @GetMapping("/products")
-    public Page<ProductResponseDto> getProducts(
-            @ModelAttribute SearchParameter parameter,
-            @AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public Page<ProductResponseDto> getProducts(@ModelAttribute SearchParameter parameter,
+                                                @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return productService.getProducts(userDetails.getUser(), parameter);
     }
 
     // 관심 상품 희망 최저가 등록하기
     @PutMapping("/products/{id}")
-    public ProductResponseDto updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
+    public ProductResponseDto updateProduct(@PathVariable Long id,
+                                            @RequestBody ProductMypriceRequestDto requestDto) {
         // 응답 보내기
         return productService.updateProduct(id, requestDto);
+    }
+
+    // 상품에 폴더 추가
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(@PathVariable Long productId,
+                          @RequestParam Long folderId,
+                          @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        productService.addFolder(productId, folderId, userDetails.getUser());
     }
 
     // 관리자 조회
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,10 +4,10 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.dto.SearchParameter;
-import com.sparta.myselectshop.entity.Product;
-import com.sparta.myselectshop.entity.User;
-import com.sparta.myselectshop.entity.UserRoleEnum;
+import com.sparta.myselectshop.entity.*;
 import com.sparta.myselectshop.naver.dto.ItemDto;
+import com.sparta.myselectshop.repository.FolderRepository;
+import com.sparta.myselectshop.repository.ProductFolderRepository;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -18,15 +18,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class ProductService {
 
     private final ProductRepository productRepository;
+    private final FolderRepository folderRepository;
+    private final ProductFolderRepository productFolderRepository;
 
     public static final int MIN_MY_PRICE = 100;
-    
+
     @Transactional
     public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
@@ -76,5 +79,31 @@ public class ProductService {
     public List<ProductResponseDto> getAllProducts() {
         List<Product> products = productRepository.findAll();
         return products.stream().map(ProductResponseDto::new).toList();
+    }
+
+    public void addFolder(Long productId, Long folderId, User user) {
+
+        // 1) 상품을 조회합니다.
+        Product findProduct = productRepository.findById(productId).orElseThrow(() ->
+                new NullPointerException("해당 상품이 존재하지 않습니다.")
+        );
+        // 2) 폴더를 조회합니다.
+        Folder findFolder = folderRepository.findById(folderId).orElseThrow(
+                () -> new NullPointerException("해당 폴더가 존재하지 않습니다.")
+        );
+        // 3) 조회한 폴더와 상품이 모두 로그인한 회원의 소유인지 확인합니다.
+        if (!findProduct.getUser().getId().equals(user.getId())
+                || !findFolder.getUser().getId().equals(user.getId())) {
+            throw new IllegalArgumentException("회원님의 관심상품이 아니거나, 회원님의 폴더가 아닙니다.");
+        }
+
+        // 중복확인
+        Optional<ProductFolder> overlapFolder = productFolderRepository.findByProductAndFolder(findProduct, findFolder);
+        if (overlapFolder.isPresent()) {
+            throw new IllegalArgumentException("중복된 폴더입니다.");
+        }
+
+        // 저장
+        productFolderRepository.save(new ProductFolder(findProduct, findFolder));
     }
 }
