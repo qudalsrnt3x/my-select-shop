a61e95808dfba849fa2a1c45d2bef4fcdfef4ebb
moonbyeongmin <qudalsrnt3x@gmail.com>
2024-11-24 20:27:13 +0900

    GlobalExceptionHandler을 통해서 전역적으로 예외처리

--- src/main/java/com/sparta/myselectshop/controller/FolderApiController.java
+++ src/main/java/com/sparta/myselectshop/controller/FolderApiController.java
@@ -32,16 +32,4 @@ public class FolderApiController {
         return folderService.getFolders(userDetails.getUser());
     }
 
-    @ExceptionHandler({IllegalArgumentException.class})
-    public ResponseEntity<RestApiException> handleException(IllegalArgumentException ex) {
-        System.out.println("FolderApiController.handleException");
-        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.BAD_REQUEST.value());
-        return new ResponseEntity<>(
-                // HTTP body
-                restApiException,
-                // HTTP status code
-                HttpStatus.BAD_REQUEST
-        );
-    }
-
 }
